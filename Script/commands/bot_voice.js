88dd33b019acc56e80361181c92dd75afef712a9
mim651445-png <mim651445@gmail.com>
2026-08-30 16:53:48 +0800

    Update bot_voice.js

--- Script/commands/bot_voice.js
+++ Script/commands/bot_voice.js
@@ -22,11 +22,11 @@ module.exports.handleEvent = async function ({
   const input = event.body.toLowerCase().trim();
 
   const botVoices = [
-    "https://files.catbox.moe/hhldox.mp3",
+    "https://files.catbox.moe/3mp5xc.mp3",
     "https://files.catbox.moe/faql6m.mp3",
     "https://files.catbox.moe/gzq54t.mp3",
     "https://files.catbox.moe/v4l3im.mp3",
-    "https://files.catbox.moe/x8ina4.mp3",
+    "https://files.catbox.moe/d80i6p.mp3",
     "https://files.catbox.moe/3u6shs.mp3"
   ];
 
