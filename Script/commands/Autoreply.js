b69cf921264c472e661d6fccfcbbe2441ca0ab81
mim651445-png <mim651445@gmail.com>
2026-09-16 20:12:51 +0800

    Update Autoreply.js

--- Script/commands/Autoreply.js
+++ Script/commands/Autoreply.js
@@ -1,12 +1,12 @@
 const fs = global.nodemodule["fs-extra"];
 const path = global.nodemodule["path"];
-const guardSpam = require("../../includes/Rahat_spt.js");
+const guardSpam = require("../../includes/hriday_spt.js");
 
 module.exports.config = {
   name: "autoreplybot",
   version: "6.0.2",
   hasPermssion: 0,
-  credits: "🔰𝐑𝐀𝐇𝐀𝐓 𝐈𝐒𝐋𝐀𝐌🔰",
+  credits: "💠 𝐇𝐑𝐈𝐃𝐎𝐘 𝐇𝐀𝐒𝐀𝐍 𝐒𝐇𝐀𝐍𝐓𝐎 💠",
   description: "Auto-response bot with specified triggers (safe version)",
   commandCategory: "No Prefix",
   usages: "[any trigger]",
@@ -25,24 +25,56 @@ module.exports.handleEvent = async function ({ api, event, Users }) {
     "kiss de": "😄 haha, এখন না পরে কথা বলি!",
     "👍": "🙉👀",
     "help": "Prefix তোমার নানি কালকে দিয়ে যাবে😊",
-    "fork2": "https://github.com/Rahat-Boss/Rahat_Bot.git",
+
+    "fork2":
+      "https://github.com/hriday-hassan-shanto",
+
     "pro": "😎 Nice vibe!",
     "🙄🙄🙄": "🙄🙄🙄",
-    "Rahat": "Bot owner😄",
-    "owner": "👑 Owner: Rahat Islam\nFacebook: https://www.facebook.com/share/17D7Ftj1ri/",
-    "admin": "🔰𝐑𝐀𝐇𝐀𝐓 𝐈𝐒𝐋𝐀𝐌🔰",
+
+    "হৃদয়":
+      "🤖 Bot Developer: হৃদয় হাসান শান্ত",
+
+    "হৃদয়":
+      "🤖 Bot Developer: হৃদয় হাসান শান্ত",
+
+    "owner":
+      "👑 Owner: হৃদয় হাসান শান্ত\n💠 Developer: Hridoy Hasan Shanto",
+
+    "admin": "💠 𝐇𝐑𝐈𝐃𝐎𝐘 𝐇𝐀𝐒𝐀𝐍 𝐒𝐇𝐀𝐍𝐓𝐎 💠",
+
     "babi": "😊 দুষ্টু তুমি",
-    "chup": "😄চুপ কীভাবে করে🙄",
-    "assalamualaikum": "وَعَلَيْكُمُ السَّلَامُ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ 💖",
+    "chup": "😄 চুপ কীভাবে করে🙄",
+
+    "assalamualaikum":
+      "وَعَلَيْكُمُ السَّلَامُ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ 💖",
+
     "kiss me": "😄 virtual hug পাঠালাম 🤗",
     "thanks": "😊 সাহায্য করতে পেরে খারাপ লাগলো",
     "i love you": "❤️ Thank you! You’re awesome",
     "by": "Bye 👋 ভালো থেকো",
-    "ami Rahat": "হ্যাঁ 😄 বলো বস",
-    "tor nam ki": "My name is 🔰 Rahat Bot 🔰",
-    "pic de": "📸 এখন ছবি শেয়ার করতে পারছি না",
-    "আমি রাহাদ": "হ্যাঁ 😄 বলো কী লাগবে?",
-    "murgi": "🐔 কাউকে মুরগি দিলে আমি লিভ নিবো😒",
+
+    "ami hriday":
+      "হ্যাঁ 😄 বলো বস",
+
+    "ami হৃদয়":
+      "হ্যাঁ 😄 বলো বস",
+
+    "tor nam ki":
+      "My name is 💠 হৃদয় হাসান শান্ত Bot 💠",
+
+    "pic de":
+      "📸 এখন ছবি শেয়ার করতে পারছি না",
+
+    "আমি হৃদয়":
+      "হ্যাঁ 😄 বলো কী লাগবে?",
+
+    "আমি হৃদয়":
+      "হ্যাঁ 😄 বলো কী লাগবে?",
+
+    "murgi":
+      "🐔 কাউকে মুরগি দিলে আমি লিভ নিবো😒",
+
     "heda": "😄 ok",
     "boda": "😄 haha",
     "love you": "❤️ love you too",
@@ -59,12 +91,21 @@ module.exports.handleEvent = async function ({ api, event, Users }) {
       messageID,
       triggerKey: "autoreply:" + msg,
     });
+
     if (spamBanned) return;
 
-    return api.sendMessage(responses[msg], threadID, messageID);
+    return api.sendMessage(
+      responses[msg],
+      threadID,
+      messageID
+    );
   }
 };
 
 module.exports.run = async function ({ api, event, args, Users }) {
-  return this.handleEvent({ api, event, Users });
+  return this.handleEvent({
+    api,
+    event,
+    Users
+  });
 };
